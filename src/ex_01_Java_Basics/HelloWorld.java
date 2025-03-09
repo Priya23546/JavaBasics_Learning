42faef9504a005acb91c2ce9b459b8a308992ca7
priyakoti <priyankamaramreddy@gmail.com>
2025-03-09 23:57:54 +0530

    It will print Hello World

---
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
