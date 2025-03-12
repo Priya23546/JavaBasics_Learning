79ec9d31f0c3940c43fac4ad1698f11dbeb58703
priyakoti <priyankamaramreddy@gmail.com>
2025-03-12 18:56:30 +0530

    It will print Hello World

---
+++ src/ex_01_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+import java.sql.SQLOutput;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        System.out.println("Hello priya");// this comments will use for only single file
+        /** it will use for multiple comments**/
+        System.out.println(3+3);
+    }
+}
